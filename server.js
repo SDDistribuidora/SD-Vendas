c070427db880f7d6ed586e36077ce1209f4ee463
Victor Diniz <victorhugo10diniz@gmail.com>
2025-08-22 13:15:04 -0300

    Corrige biblioteca dos Correios e lógica de caixa

--- server.js
+++ server.js
@@ -98,4 +98,5 @@ app.post('/calcular-frete-e-pagamento', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
+    
 });
